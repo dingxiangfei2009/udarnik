29718fa5a121a9645adf55e63e58d7913c471daa
Ding Xiang Fei <dingxiangfei2009@protonmail.ch>
2020-01-23 15:16:15 +0800

    wip

--- src/bin/tun.rs
+++ src/bin/tun.rs
@@ -1,6 +1,12 @@
 use udarnik::tun::linux::UtunDev;
+use futures::{prelude::*, executor::block_on};
 
 fn main() {
-    let (dev, _, _) = UtunDev::new("tun%d").unwrap();
-    println!("{:?}", dev.name());
+    let _guard = slog_envlogger::init();
+    let (dev, tx, mut rx) = UtunDev::new("tun%d").unwrap();
+    block_on(async move {
+        while let Some(packet) = rx.next().await {
+            println!("pkt={:?}", packet);
+        }
+    });
 }
